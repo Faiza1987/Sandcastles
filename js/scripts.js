e6c7672f7a60cf3b04f730c48eaf21c8070c590b
Faiza Husain and Nanette Girzi <nanette@>
2018-01-09 13:14:11 -0800

    Removed 2 sections and added clickable class.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $("#castle4-showing").fadeToggle();
-    $("#castle4").fadeToggle();
+    $("#castle4-hidden").fadeToggle();
   });
 
   $(".clickable").click(function() {
     $("#castle3-showing").slideToggle();
-    $("#castle3").slideToggle();
+    $("#castle3-hidden").slideToggle();
   });
 
   $(".clickable").click(function() {
     $("#castle2-showing").Toggle();
-    $("#castle2").Toggle();
+    $("#castle2-hidden").Toggle();
   });
 
   $(".clickable").click(function() {
     $("#castle1-showing").Toggle();
-    $("#castle1").Toggle();
+    $("#castle1-hidden").Toggle();
   });
 });
